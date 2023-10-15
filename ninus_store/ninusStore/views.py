15bdbd0fbefd20f98ff8fef637f44f1230fb7939
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-16 01:18:52 +1100

    updating catalogue v1
    updating catalogue v1

--- ninus_store/ninusStore/views.py
+++ ninus_store/ninusStore/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"home.html")
+    return render(request,"catalogue.html")
--- ninus_store/ninus_store/settings.py
+++ ninus_store/ninus_store/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR/"static"]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
