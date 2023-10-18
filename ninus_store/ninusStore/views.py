0a6d33a10d096eff8f8c99995bfa5a5b3f5d2fd6
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-19 02:03:04 +1100

    update mini product detail

--- ninus_store/ninusStore/views.py
+++ ninus_store/ninusStore/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"product_detail.html")
+    return render(request,"product_detail_mini.html")
