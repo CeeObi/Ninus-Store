95a77b7df2ed6dc23c6ccbae7d9c309ff3aeaddf
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-21 21:54:08 +1100

    update page links

--- ninus_store/ninusStore/urls.py
+++ ninus_store/ninusStore/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path('',views.index,name="home")
+  path('',views.index,name="home"),
+  path('catalog',views.catalog,name="catalog"),
+  path('detail-x',views.product_detail,name="product-x"),
+
 ]
--- ninus_store/ninusStore/views.py
+++ ninus_store/ninusStore/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"header.html")
+    return render(request,"home.html")
+
+def catalog(request):
+    return render(request,"catalog.html")
+
+def product_detail(request):
+    return render(request,"product_detail.html")
