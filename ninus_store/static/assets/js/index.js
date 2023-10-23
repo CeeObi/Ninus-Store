9efc3fb1b9acbe8c5dac46604ebcb73a9d1d9ccb
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-23 23:26:07 +1100

    update catalog data v2

--- ninus_store/static/assets/js/index.js
+++ ninus_store/static/assets/js/index.js
@@ -1,7 +1,16 @@
+alert("Ready?");
 document.querySelector("h1").style.fontSize="70px";
+document.querySelector("h1").style.color="purple";
 
 function toToggle(){
     document.querySelector(".product-hover-overlay").classList.toggle("visible")
 };
 document.querySelector(".product-lists").addEventListener("mouseenter", toToggle);
 document.querySelector(".product-lists").addEventListener("mouseleave", toToggle);
+
+
+$('#pixz_source').on( "click", function(){
+    var _data = $("#pixz_sorc2").attr('src');
+    console.log(_data)
+    $('#pixz_srcs3').attr('src' , _data);
+});
