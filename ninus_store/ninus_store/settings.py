a8d503b5b10469d64b65651f832d8ac6c14f7626
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-24 17:09:23 +1100

    updated modal pics

--- ninus_store/ninus_store/settings.py
+++ ninus_store/ninus_store/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ninus_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR/"static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT , 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
