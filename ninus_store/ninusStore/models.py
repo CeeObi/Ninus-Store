01837171b35c78fb8759a2be0834db9ccc49184c
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-10-26 12:17:21 +1100

    update models

--- ninus_store/ninusStore/models.py
+++ ninus_store/ninusStore/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-# class User(models.Model):
-#     username = models.CharField
+class Collections(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    order_date = models.DateTimeField()
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    collection = models.ForeignKey(Collections, on_delete=models.CASCADE)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    price=models.FloatField(max_length=255)
+    quantity = models.IntegerField()
+
+
+
