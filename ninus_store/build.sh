31eee332c0d93fd6b66a08b70cfd7b581eaec6ac
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-02 21:53:29 +1100

    update buildsh v3

--- ninus_store/build.sh
+++ ninus_store/build.sh
@@ -9,3 +9,5 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 
 python manage.py migrate
+
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
