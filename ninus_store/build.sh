236588ba64af32a706ea6c3f14faf35371faa852
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-02 21:59:50 +1100

    update buildsh v4

--- ninus_store/build.sh
+++ ninus_store/build.sh
@@ -10,4 +10,4 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser
